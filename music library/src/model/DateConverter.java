71b968de4a8075a33b229daa9e80174b0b36e634
Unai <unain@192.168.0.26>
2019-05-21 16:58:39 +0200

    Added new class to transform different types of dates

---
+++ music library/src/model/DateConverter.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateConverter {
+	public LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+	    return dateToConvert.toInstant()
+	      .atZone(ZoneId.systemDefault())
+	      .toLocalDate();
+	}
+	
+	public LocalDateTime convertToLocalDateTimeViaInstant(Date dateToConvert) {
+	    return dateToConvert.toInstant()
+	      .atZone(ZoneId.systemDefault())
+	      .toLocalDateTime();
+	}
+	
+	public Date convertToDateViaInstant(LocalDate dateToConvert) {
+	    return java.util.Date.from(dateToConvert.atStartOfDay()
+	      .atZone(ZoneId.systemDefault())
+	      .toInstant());
+	}
+	
+	public Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+	    return java.util.Date
+	      .from(dateToConvert.atZone(ZoneId.systemDefault())
+	      .toInstant());
+	}
+	
+	public java.sql.Date convertToSQLDate(LocalDate dateToConvert) {
+		return java.sql.Date.valueOf(dateToConvert);
+	}
+}
